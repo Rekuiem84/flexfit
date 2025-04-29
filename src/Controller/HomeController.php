71f8eff6af236bca262c8d9881aad77de696e54d
Rekuiem84 <morinijoshua@gmail.com>
2025-04-29 09:52:52 +0200

    restore

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -13,6 +14,27 @@ final class HomeController extends AbstractController
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+        ]);
+    }
+    #[Route('/collections', name: 'app_collections')]
+    public function collections(): Response
+    {
+        return $this->render('home/collections.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+    #[Route('/collections/{id}', name: 'app_collections_show', methods: ['GET'])]
+    public function collections_show(): Response
+    {
+        return $this->render('home/collection_show.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+    #[Route('/products', name: 'app_home_products')]
+    public function product(ProductRepository $productRepository): Response
+    {
+        return $this->render('home/products.html.twig', [
+            'products' => $productRepository->findAll(),
         ]);
     }
 }
